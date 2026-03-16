574cb9465a2dab37bce3e37704632785dc578b66
Elias Dev <elias.m.dev@gmail.com>
2026-03-16 11:15:38 -0300

    Strategy

---
+++ Fase3/Atividade StrategySLN/Atividade Strategy/CalculadoraViagem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Strategy
+{
+    internal class CalculadoraViagem
+    {
+        private ITransporteStrategy _strategy;
+
+       
+        public void DefinirEstrategia(ITransporteStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void Calcular()
+        {
+            if (_strategy == null)
+            {
+                Console.WriteLine("Por favor, selecione um transporte primeiro.");
+                return;
+            }
+            _strategy.CalcularTempo();
+        }
+    }
+}
---
+++ Fase3/Atividade StrategySLN/Atividade Strategy/Program.cs
@@ -0,0 +1,19 @@
+﻿using Atividade_Strategy;
+class Program
+{
+    static void Main()
+    {
+        var calculadora = new CalculadoraViagem();
+
+
+        calculadora.DefinirEstrategia(new BicicletaStrategy());
+        calculadora.Calcular();
+
+        calculadora.DefinirEstrategia(new CarroStrategy());
+        calculadora.Calcular();
+
+       
+        calculadora.DefinirEstrategia(new AviaoStrategy());
+        calculadora.Calcular();
+    }
+}
